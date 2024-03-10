8961345747d33e9e8370afe1aaa16ab14818229f
Robert Mikolajczyk <ronert1674@gmail.com>
2024-03-10 19:03:36 +0100

    add program starting point

---
+++ src/main.py
@@ -0,0 +1,12 @@
+"""..."""
+from textnode import TextNode
+
+
+def main():
+    """..."""
+    text_node = TextNode("This is a text node", "bold", "https://www.google.com")
+    print(text_node)
+
+
+if __name__ == "__main__":
+    main()
