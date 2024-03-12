30125838f6c5beb3e6267ab4fef1296a16b8e9c9
Robert Mikolajczyk <ronert1674@gmail.com>
2024-03-12 10:35:51 +0100

    add convert text node to leaf node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,13 @@
 """..."""
+from htmlnode import LeafNode
+
+
+TEXT_TYPE_TEXT = "text"
+TEXT_TYPE_BOLD = "bold"
+TEXT_TYPE_ITALIC = "italic"
+TEXT_TYPE_CODE = "code"
+TEXT_TYPE_LINK = "link"
+TEXT_TYPE_IMAGE = "image"
 
 
 class TextNode:
@@ -17,3 +26,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    """..."""
+    if text_node.text_type == TEXT_TYPE_TEXT:
+        return LeafNode(tag=None, value=text_node.text)
+    if text_node.text_type == TEXT_TYPE_BOLD:
+        return LeafNode(tag="b", value=text_node.text)
+    if text_node.text_type == TEXT_TYPE_ITALIC:
+        return LeafNode(tag="i", value=text_node.text)
+    if text_node.text_type == TEXT_TYPE_CODE:
+        return LeafNode(tag="code", value=text_node.text)
+    if text_node.text_type == TEXT_TYPE_LINK:
+        return LeafNode(tag="a", value=text_node.text,
+                        props={"href": text_node.url})
+    if text_node.text_type == TEXT_TYPE_IMAGE:
+        return LeafNode(tag="img", value="",
+                        props={"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"{text_node.text_type} is not supported type")
