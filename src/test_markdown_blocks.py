3569aa3c0e6a06b07c03d9288a3321916cb08cae
Robert Mikolajczyk <ronert1674@gmail.com>
2024-04-02 12:34:54 +0200

    add test for checking type of blok in the markdown

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,16 @@
 """..."""
 import unittest
 from markdown_bloks import markdown_to_blocks
+from markdown_bloks import block_to_block_type
+
+from markdown_bloks import (
+    BLOCK_TYPE_CODE,
+    BLOCK_TYPE_HEADING,
+    BLOCK_TYPE_ORDERED_LIST,
+    BLOCK_TYPE_PARAGRAPH,
+    BLOCK_TYPE_QUOTE,
+    BLOCK_TYPE_UNORDERED_LIST,
+)
 
 
 class TestMarkdownToHTML(unittest.TestCase):
@@ -47,6 +57,24 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+
+class TestBlockType(unittest.TestCase):
+    """Class represenating tests related to types of markdown"""
+    def test_block_to_blok_type(self):
+        """Test makdown blokc type beased on patterns"""
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_QUOTE)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_UNORDERED_LIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_ORDERED_LIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BLOCK_TYPE_PARAGRAPH)
 
 
 if __name__ == "__main__":
