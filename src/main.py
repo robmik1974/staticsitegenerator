647c766f2e79e23d4cb8a2c6383b7a5614cfb906
Robert Mikolajczyk <ronert1674@gmail.com>
2024-04-06 16:21:09 +0200

    update main.py file to support copying files' structure

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 """..."""
 from textnode import TextNode
+from copy_folder_structure import (
+    prepare_folder_structure,
+    copy_folder_structure
+)
+
+SOURCE_PATH = "./static"
+DEST_PATH = "./public"
 
 
 def main():
     """..."""
     text_node = TextNode("This is a text node", "bold", "https://www.google.com")
     print(text_node)
+    prepare_folder_structure(DEST_PATH)
+    copy_folder_structure(SOURCE_PATH, DEST_PATH)
 
 
 if __name__ == "__main__":
