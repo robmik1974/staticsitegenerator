7c40128881a1b8f110bd85cc332fe0ef176ffd9d
Robert Mikolajczyk <ronert1674@gmail.com>
2024-04-11 19:42:35 +0200

    update main.py to start generating html page

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
 """..."""
-from textnode import TextNode
+import os
+from generate_page import generate_page
 from copy_folder_structure import (
     prepare_folder_structure,
     copy_folder_structure
 )
 
 SOURCE_PATH = "./static"
+TEMPLATE_PATH = "./template.html"
 DEST_PATH = "./public"
+CONTENT_PATH = "./content"
 
 
 def main():
     """..."""
-    text_node = TextNode("This is a text node", "bold", "https://www.google.com")
-    print(text_node)
     prepare_folder_structure(DEST_PATH)
     copy_folder_structure(SOURCE_PATH, DEST_PATH)
+    generate_page(
+        os.path.join(CONTENT_PATH, "index.md"),
+        TEMPLATE_PATH,
+        os.path.join(DEST_PATH, "index.html")
+    )
 
 
 if __name__ == "__main__":
